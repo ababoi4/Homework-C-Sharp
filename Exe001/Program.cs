024bb96b7da5ea6df4ffc92b71dd83f5aaf3e664
ababoi4 <105204537+ababoi4@users.noreply.github.com>
2022-08-06 21:02:07 +0300

    Init Exe001

---
+++ Exe001/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача № 1 
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readVelue = Console.ReadLine();
+    int result = int.Parse(readVelue);
+    return result;
+}
+var value = Prompt("Введите число >");
+var valueNew = Prompt("Введите второе число >");
+if (value > valueNew)
+{
+    System.Console.WriteLine(value);
+    System.Console.WriteLine(valueNew);
+}
+else
+{
+    System.Console.WriteLine(valueNew);
+    System.Console.WriteLine(value);
+};
